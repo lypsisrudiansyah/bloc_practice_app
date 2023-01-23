94d9bb2aab8a5d6e4797dfdf0d471f67a0cd369a
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-01-23 11:02:46 +0700

    feat: add MultiBlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:practice_bloc_app/bloc/shop_app/product/product_bloc.dart';
+import 'package:practice_bloc_app/cubit/shop_app/product/product_cubit.dart';
 import 'package:practice_bloc_app/screen/home_screen.dart';
 import 'package:practice_bloc_app/theme.dart';
 
@@ -12,13 +15,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Simple Shop App',
-      theme: ThemeData(
-        primaryColor: primaryColorCustom,
-        colorScheme: ColorScheme.fromSwatch().copyWith(primary: primaryColorCustom),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => ProductBloc()),
+        BlocProvider(create: (context) => ProductCubit()),
+      ],
+      child: MaterialApp(
+        title: 'Simple Shop App',
+        theme: ThemeData(
+          primaryColor: primaryColorCustom,
+          colorScheme: ColorScheme.fromSwatch().copyWith(primary: primaryColorCustom),
+        ),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
