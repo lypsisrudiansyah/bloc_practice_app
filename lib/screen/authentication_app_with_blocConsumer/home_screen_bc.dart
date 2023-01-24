3debd81175f2da6b3c65bdb73aa5951014139e14
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-01-24 21:53:39 +0700

    feat: create bc=BlocConsumer.txt, home_screen_bc.dart and login_screen_bc.dart

---
+++ lib/screen/authentication_app_with_blocConsumer/home_screen_bc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomaScreenBc extends StatelessWidget {
+  const HomaScreenBc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
---
+++ lib/screen/authentication_app_with_blocConsumer/login_screen_bc.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:practice_bloc_app/screen/authentication_app_with_blocConsumer/home_screen_bc.dart';
+
+class LogiScreenBc extends StatefulWidget {
+  const LogiScreenBc({super.key});
+
+  @override
+  State<LogiScreenBc> createState() => _LogiScreenBcState();
+}
+
+class _LogiScreenBcState extends State<LogiScreenBc> {
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    userNameController.clear();
+    passwordController.clear();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              TextField(
+                controller: userNameController,
+                decoration: const InputDecoration(
+                  label: Text('username'),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  label: Text('password'),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomaScreenBc(),
+                    ),
+                  );
+                },
+                child: const Text('Sign In'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
